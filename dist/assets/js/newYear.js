854f00c84ae7cdd6ba906a71e1b776d92549262a
petrashalla <168079826+petrashalla@users.noreply.github.com>
2024-09-17 16:22:28 +0300

    Delete dist directory

--- dist/assets/js/newYear.js
+++
@@ -1,9 +0,0 @@
-const headerTop = document.querySelector('.header__top-wrap');
-if (headerTop) {
-  headerTop.classList.add('header__icicles-wrapper_mobile');
-  headerTop.classList.add('header__icicles-wrapper');
-  const mobileIcicle = document.createElement('div');
-  mobileIcicle.classList.add('header__icicles');
-  mobileIcicle.classList.add('header__icicles_mobile');
-  headerTop.appendChild(mobileIcicle);
-}
